606d14bfdf4f1a97ceea41f583f8f44ebee5b28f
Moinul Islam <moinulilm10@gmail.com>
2024-05-31 20:16:31 +0600

    "Refactor index.tsx page, replacing original content with new layout and components"

---
+++ src/components/ArrowIcon.tsx
@@ -0,0 +1,30 @@
+import { AppContext } from "@/pages/_app";
+import { useContext } from "react";
+import { BsChevronUp } from "react-icons/bs";
+
+const ArrowIcon = () => {
+  const { state, setState } = useContext(AppContext);
+
+  return (
+    <BsChevronUp
+      onClick={() =>
+        state.displayGuide
+          ? setState({
+              ...state,
+              displayGuide: false,
+            })
+          : setState({
+              ...state,
+              displayGuide: true,
+            })
+      }
+      style={{
+        transform: state.displayGuide ? "rotate(0)" : "rotate(180deg)",
+        fontSize: "2rem",
+        cursor: "pointer",
+      }}
+    />
+  );
+};
+
+export default ArrowIcon;
